627f3a6f2a8c29e618e39b561f8eb884b9b81b1a
micnic <micnic90@gmail.com>
2018-04-29 16:58:36 +0300

    get proposals

--- static/js/components/App.js
+++ static/js/components/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import Api from '../api';
+
 export default class App extends React.Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			loaded: false,
+			proposals: null
+		};
+	}
+
+	componentDidMount() {
+		Api.getContract().then(() => Api.getProposals()).then((proposals) => {
+			this.setState({
+				loaded: true,
+				proposals
+			});
+		}).catch((error) => {
+			console.log('Could not load abi');
+		});
+	}
 
 	render() {
 
-		return React.createElement(React.Fragment, null, 'This is the main component');
+		if (this.state.loaded) {
+			return React.createElement(React.Fragment, null, `Here are the proposals: ${JSON.stringify(this.state.proposals)}`);
+		} else {
+
+			return React.createElement(React.Fragment, null, 'Loading...');
+		}
 	}
 }
